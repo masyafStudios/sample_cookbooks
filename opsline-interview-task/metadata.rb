17fa8f3d892e3e735d3b50c7756fb772606be3fa
Kenny Raghunath <Kenny.Raghunath@gmail.com>
2016-06-15 14:15:40 +0000

    opsline-interview-task 0.0.15 - Froze cookbooks in metadata.rb.

--- opsline-interview-task/metadata.rb
+++ opsline-interview-task/metadata.rb
@@ -1,7 +1,7 @@
 name	'opsline-interview-task'
-version	'0.0.14'
+version	'0.0.15'
 
-depends 'apache2'
-depends 'mysql'
-depends 'php'
-depends 'chef-vault'
+depends 'apache2', '~> 3.2.2'
+depends 'mysql', '~> 7.1.1'
+depends 'php', '~> 1.9.0'
+depends 'chef-vault', '~> 1.3.3'
